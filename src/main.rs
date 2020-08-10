f6d6248c3814ed5a0810e1db2f89d6a9ce466a97
Arman <armanshah360@gmail.com>
2020-08-09 22:49:58 -0700

    Use mpsc channels to communicate between threads

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use pipeviewer::{args::Args, read, stats, write};
+use std::io::Result;
+use std::sync::mpsc;
+use std::thread;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
+
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
+
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, write_tx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
+
+    // crash if any threads have crashed
+    // `.join()` returns a `thread::Result<io::Result<()>>`
+    let read_io_result = read_handle.join().unwrap();
+    let stats_io_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
+
+    read_io_result?;
+    stats_io_result?;
+    write_io_result?;
+
+    Ok(())
+}
